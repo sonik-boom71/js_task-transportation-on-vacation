ba1bfe8b3ebf24a21647072cad87836d85e747c6
Maksym Davydiuk <krytan53@gmail.com>
2026-03-16 14:49:51 +0200

    Implement calculateRentalCost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,21 @@
-/**
- * @param {number} days
- *
- * @return {number}
- */
 function calculateRentalCost(days) {
-  // write code here
+  const DAILY_PRICE = 40;
+  const BASIC_DISCOUNT_DAYS = 3;
+  const BASIC_DISCOUNT = 20;
+  const EXTRA_DISCOUNT_DAYS = 7;
+  const EXTRA_DISCOUNT = 50;
+
+  const totalPrice = days * DAILY_PRICE;
+
+  if (days >= EXTRA_DISCOUNT_DAYS) {
+    return totalPrice - EXTRA_DISCOUNT;
+  }
+
+  if (days >= BASIC_DISCOUNT_DAYS) {
+    return totalPrice - BASIC_DISCOUNT;
+  }
+
+  return totalPrice;
 }
 
 module.exports = calculateRentalCost;
